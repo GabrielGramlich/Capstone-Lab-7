ca83b4d20bc700639f31171d5da1920f2dbd79b1
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-02-24 19:20:14 -0600

    Add comments about logging

--- weather_forecast.py
+++ weather_forecast.py
@@ -1,10 +1,4 @@
 '''
-Use the forecast API to create a detailed, neatly formatted 5-day forecast, for anywhere the user chooses. Ask the user for the location.
-
-Make sure your API key is not coded into your program. Your program should read the key from an environment variable. 
-
-Use a query parameter dictionary in the request.
-
 Your forecast should show the temperature and unit (your choice of F or C), weather description, and wind speed for every three hour interval, over the next 5 days.
 
 Your program should handle errors. What type of errors do you anticipate? How will you deal with them?
@@ -30,6 +24,9 @@ def main():
 		items = parse_data(data)
 		display_weather_information(items)
 	else:
+		'''I imagine I would want to log here, and as
+		 CRITICAL, and let the user know the program
+		 won't work, so sorry.'''
 		raise WeatherError('API Key is missing')
 
 
@@ -60,6 +57,10 @@ def parse_data(forecast_items):
 
 		return items
 	except KeyError:
+		'''Here I imagine there's not really much point in logging,
+		 but maybe it's not an issue of user input, so log it as
+		 INFO, maybe? Still let the user know it's an invalid
+		 selection, cause then they can try something different.'''
 		raise WeatherError('Invalid selection.')
 
 
